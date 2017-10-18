ab7a2c35ae33c9464d37b56ebd77c0e19f848edc
welcome <welcomeking@outlook.com>
2017-10-18 15:33:15 +0200

    Updated and added libs
    Google ,  mikepenZ , jakewharton , fontawesome , azure
    ,play-services-maps

---
+++ app/src/main/java/com/daedalusdigital/imakapp/SplashActivity.java
@@ -0,0 +1,73 @@
+package com.daedalusdigital.imakapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.daedalusdigital.imakapp.utils.LogUtil;
+
+import butterknife.ButterKnife;
+
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    private Class<?> mMainActivity;
+    private AsyncTask<Void, Void, Boolean> mPrepareApp;
+
+    @Deprecated
+    public void initSplashActivity(@Nullable Bundle savedInstanceState, @NonNull Class<?> mainActivity, int duration) {
+        initSplashActivity(savedInstanceState, mainActivity);
+    }
+
+    public void initSplashActivity(@Nullable Bundle savedInstanceState, @NonNull Class<?> mainActivity) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mMainActivity = mainActivity;
+        prepareApp();
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+        if (mPrepareApp != null) mPrepareApp.cancel(true);
+        super.onDestroy();
+    }
+
+    private void prepareApp() {
+        mPrepareApp = new AsyncTask<Void, Void, Boolean>() {
+            @Override
+            protected Boolean doInBackground(Void... voids) {
+                while (!isCancelled()) {
+                    try {
+                        Thread.sleep(500);
+                        return true;
+                    } catch (Exception e) {
+                        LogUtil.e(Log.getStackTraceString(e));
+                        return false;
+                    }
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Boolean aBoolean) {
+                super.onPostExecute(aBoolean);
+                mPrepareApp = null;
+                if (aBoolean) {
+                    startActivity(new Intent(SplashActivity.this, mMainActivity));
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                    finish();
+                }
+            }
+        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+}
